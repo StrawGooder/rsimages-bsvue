5db3cf8f67d3ed516d3441d9cd50251f822b82a9
straw <strawtobetter@163.com>
2022-12-13 00:20:00 +0800

    add pages plugins vue.config.js

---
+++ src/plugins/bootstrapVue.js
@@ -0,0 +1,9 @@
+import {BootstrapVue, IconsPlugin} from "bootstrap-vue"
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+    "bsvue":BootstrapVue,
+    "bsvue-icon-plugin":IconsPlugin
+}
---
+++ src/plugins/index.js
@@ -0,0 +1,7 @@
+import Vue from "vue"
+import bootstrapVue from "./bootstrapVue.js"
+
+Vue.use(bootstrapVue["bsvue"])
+Vue.use(bootstrapVue["bsvue_icon_plugin"])
+
+console.log("install vue plugins")
